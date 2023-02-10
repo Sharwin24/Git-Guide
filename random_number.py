6656a4c0050f453c3ba50278733029f8b8a915cb
Sharwin Patil <sharwin24@gmail.com>
2023-02-10 00:30:57 -0500

    Multiplied output

--- random_number.py
+++ random_number.py
@@ -1,4 +1,9 @@
 import random
 
+# Generate a random number between 0 and 100 and print it
 random_number = random.randint(0, 100)
-print(random_number)
+# print(random_number)
+
+# Generate another random number between 0 and 6
+# and multiply it by our previous number
+print(random_number * random.randint(0, 6))
